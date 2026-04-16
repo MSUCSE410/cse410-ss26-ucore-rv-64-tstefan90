b137d54a6273445e7b7fd274edadf93f7e61adee
tstefan90 <163595386+tstefan90@users.noreply.github.com>
2026-04-16 23:25:28 +0000

    final merge

--- os/syscall.c
+++ os/syscall.c
@@ -190,16 +190,38 @@ uint64 sys_wait(int pid, uint64 va)
 	return wait(pid, code);
 }
 
-uint64 sys_spawn(uint64 va)
+uint64 sys_spawn(uint64 path, uint64 uargv)
 {
-	// TODO: your job is to complete the sys call
-	return -1;
+	struct proc *p = curr_proc();
+	char name[MAX_STR_LEN];
+	if (copyinstr(p->pagetable, name, path, MAX_STR_LEN) != 0) {
+		return -1;
+	}
+	uint64 arg;
+	static char strpool[MAX_ARG_NUM][MAX_STR_LEN];
+	char *argv[MAX_ARG_NUM];
+	int i;
+	for (i = 0; uargv && (arg = fetchaddr(p->pagetable, uargv));
+	     uargv += sizeof(char *), i++) {
+		if (copyinstr(p->pagetable, (char *)strpool[i], arg, MAX_STR_LEN) != 0) {
+			return -1;
+		}
+		argv[i] = (char *)strpool[i];
+	}
+	argv[i] = NULL;
+	return spawn(name, (char **)argv);
 }
 
-uint64 sys_set_priority(long long prio)
+uint64 sys_set_priority(long long prio) 
 {
-	// TODO: your job is to complete the sys call
-	return -1;
+	struct proc *p = curr_proc();
+	if (prio < 2 || prio > __INT_MAX__) // check if priority is valid
+	{
+		return -1;
+	}
+	p->priority = prio;
+	p->pass = BIG_STRIDE / prio;
+    return prio;
 }
 
 uint64 sys_openat(uint64 va, uint64 omode, uint64 _flags)
@@ -411,7 +433,7 @@ void syscall()
 	case SYS_unlinkat:
 	    ret = sys_unlinkat(args[0],args[1],args[2]);
 	case SYS_spawn:
-		ret = sys_spawn(args[0]);
+		ret = sys_spawn(args[0], args[1]);
 		break;
 	case SYS_setpriority:
 		ret = sys_set_priority(args[0]);
